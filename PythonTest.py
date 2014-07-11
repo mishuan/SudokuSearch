5ee0abc85aa0635a06523bf26f655be13fe64067
DrCalculus <yuanshihao1994@gmail.com>
2014-07-11 16:17:46 -0400

    Some progress -update grid needs fixing -other than that, i think we are gucci

--- PythonTest.py
+++ PythonTest.py
@@ -20,16 +20,39 @@ if __name__ == '__main__':
     
     temp = Test()
     modList(temp)
-    print temp.listVar
+    print 'Removed 2.', temp.listVar
     modInt(temp)
-    print temp.intVar
-    print temp.listVar
+    print 'First variable of list become 1. ', temp.listVar, temp.intVar
     tempBool = recurseNreturn(temp, 0)
     print tempBool
     print temp.listVar
-    
+    '''
+    Some set tests
+    '''
     set1 = set([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
     set2 = [1, 4, 3, 2, 5]
     set1 = set1.difference(set2)
     print set1
+    '''
+    Exploring list of objects and references
+    '''
+    listA = [0]
+    listB = listA
+    listB.append(1)
+    print listA
+    objList = [Test(), Test()]
+    print objList
+    temp = objList[1]
+    objList[1] = Test()
+    print objList
+    objList[1] = temp
+    print objList
+    
+    foo = objList[1]
+    foo.listVar[0] = 100
+    l = foo.listVar
+    l[1] = 200
+    print objList[1].listVar
+    print (7 / 5) * 5
+      
     pass
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import copy
+
 class Entry:
     '''
     Entry for every square
@@ -6,10 +8,68 @@ class Entry:
         self.options = set()
         self.num = i
         
-def search(grid):
+def search(grid, mappings, index):
+    if index == len(mappings):
+        solution = []
+        for entry in grid:
+            solution.append(entry.num)
+        print solution
+        return True 
     
-    pass
+    if index > len(mappings):
+        return False;
+    
+    currentEntry = mappings[index][0]
+    
+    if not grid[currentEntry].options:
+        return False;
+    
+    '''
+    I could also update mappings after every successful iteration, but the algorithm
+    is already more complex than it needs to be for such a small state problem.
+    '''
+    for option in grid[currentEntry].options:
+        gridCopy = copy.deepcopy(grid)
+        gridCopy[currentEntry].num = option
+        updateGrid(gridCopy, currentEntry, option)
+        
+        if search(gridCopy, mappings, index + 1) == True:
+            grid = gridCopy
+            return True;
 
+    return False
+
+def updateGrid(grid, currentEntry, option):
+    row = currentEntry / 9
+    col = currentEntry % 9
+    offset = (row / 3) * 3 + (col / 3) * 27
+    print row, col
+    for j in range(9):
+        '''
+        I think this works
+        '''
+        l1 = grid[row * 9 + j].options
+        l2 = grid[col + 9 * j].options
+        if not l1:
+            for i, n in enumerate(l1):
+                if n == option:
+                    del l1[i]
+            
+        if not l2:
+            for i, n in enumerate(l2):
+                if n == option:
+                    del l2[i]
+        
+        jRow = j / 3
+        jCol = j % 3
+        print jRow * 9 + jCol + offset, offset
+        lb = grid[jRow * 9 + jCol + offset].options
+        if not lb:
+            for i, n in enumerate(lb):
+                if n == option:
+                    del lb[i]
+            
+                    
 def sameRow(i, j): 
     return (i / 9 == j / 9)
 
@@ -20,7 +80,6 @@ def sameBlock(i, j):
     return (i / 27 == j / 27 and i % 9 / 3 == j % 9 / 3)
 
 def setupGrid(grid):
-    count = 0
     for i, entry in enumerate(grid):
         if entry.num != '0':
             continue
@@ -32,24 +91,38 @@ def setupGrid(grid):
         tempList = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
         tempSet = set([n for n in tempList if n not in entry.options])
         entry.options = tempSet
-        count += 1
-    return count
-    
+
+def getGridMappings(grid):
+    mappings = []
+    for i, entry in enumerate(grid):
+        if entry.num == '0':
+            obj = (i, len(entry.options)) 
+            mappings.append(obj)
+    mappings.sort(key=lambda x:x[1])
+    return mappings
+
 if __name__ == '__main__':
-    
+    '''
+    Read file and create list
+    '''
     inputFile = raw_input("Enter your input Sudoku file: ");
     fo = open(inputFile)
-    '''
-    lines = [line.strip() for line in open(inputFile)]
-    grid = [map(int, str(line)) for line in lines]
-    print grid
-    '''
     line = fo.readline()
     fo.close()
     print line
+    '''
+    Setup list and create mappings for zero entries
+    '''
     grid = [Entry(i) for i in line]
-    emptySquares = setupGrid(grid)
-    for entry in grid:
-        print entry.options
-    print emptySquares
+    setupGrid(grid)
+    mappings = getGridMappings(grid)
+    print mappings
+    '''
+    Search for solution
+    '''
+    solution = []
+    if search(grid, mappings, 0) == True:
+        for entry in grid:
+            solution.append(entry.num)
+    print solution
     pass
